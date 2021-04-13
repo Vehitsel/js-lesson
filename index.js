f660ccf324286704e4e31e702c3fe3c089b5d3bd
vehitsel <vehitsel@gmail.com>
2021-04-13 14:27:21 +0300

    init comit

---
+++ index.js
@@ -0,0 +1,70 @@
+//imports
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+
+//init zone
+const app = express();
+app.use(cors());
+
+app.post('/testPost', (req, res) => {
+    res.status(200).json({message: 'It is POST'})
+})
+
+app.get('/testGet', (req, res) => {
+    res.status(200).json({ message: 'It is GET' });
+})
+
+app.delete('/testDelete', (req, res) => {
+    res.status(400).json({message: 'Sorry, this method not work'})
+})
+
+app.put('/testPut', (req, res) => {
+    res.status(500).json({message: 'Server error'})
+})
+
+app.patch('/testPatch', (req, res) => {
+    res.status(403).json({message: 'Your token incorrect'})
+})
+
+app.post('/addToDo', (req, res) => {
+    res.status(200).json({message: 'Add ToDo success'})
+})
+
+app.delete('/deleteToDo', (req, res) => {
+    res.status(200).json({message: 'Delete ToDo success'})
+})
+
+app.post('/updateToDo', (req, res) => {
+    res.status(200).json({message: 'Update ToDo success'})
+})
+
+app.get('/todoList', (req, res) => {
+
+	let todolist = []
+	for (let i = 0; i < 10; i++){
+		let todo = {
+			id: i,
+			title: "title = " + i,
+			description: `descr ${i}`,
+		}
+	}
+	res.status(200).json({todolist})
+    //Добавляем метод: GET /todoList, ответ: 200, { todoList: [...] }. Модель одной ToDo: { _id: ‘...’, title: ‘...’, description: ‘...’ }. Список должен генериться методом for, где toDo._id = index.    
+})
+
+http.createServer(app).listen(3000, () => {
+    console.log('Hello');
+})
+
+//request
+// app.all("/", (req, res) => {
+// app.all('/', (req, res) => {
+//     console.log('URL = ',req.url);
+//     console.log('Method = ', req.method);
+//     console.log('IsSecure = ' ,req.secure);
+//     console.log('Query = ' ,req.query);
+//     console.log('Body = ', req.body);
+//     console.log('HI!');
+//     res.status(200).json({message:'All is ok!'});
+// })
